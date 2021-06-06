58963def40589fe99ae7d0d1b76cdd1565c53424
Ilya Azin <martis.azin@gmail.com>
2021-06-06 22:25:13 +0300

    feat(todo-app): init structure (step 2)

---
+++ todo-app/src/app/index.tsx
@@ -0,0 +1,13 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Todo App
+      </header>
+    </div>
+  );
+}
+
+export default App;
